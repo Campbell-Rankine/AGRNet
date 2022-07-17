bf359ac7770313f738cfea6d53e31b1f942ccba1
Anonymous would rather not say <campbellrankine@gmail.com>
2022-07-17 21:45:43 +1000

    ordering pt 2

--- Train.py
+++ Train.py
@@ -273,10 +273,10 @@ def Train():
 
                 eps = T.rand(samples.size(0), 1, 1, 1, device=device)
                 x_hat = eps * samples + (1 - eps) * fake.detach()
+                x_hat.requires_grad = True
                 with T.no_grad():
                     eps = eps.expand_as(Gen(noise))
                     px_hat = Disc(x_hat)
-                x_hat.requires_grad = True
                 grad = T.autograd.grad(
                                             outputs = px_hat.sum(),
                                             inputs = x_hat, 

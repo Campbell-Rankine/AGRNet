e846a299a893b41da27df3507eb021037b8118a2
Anonymous would rather not say <campbellrankine@gmail.com>
2022-07-17 22:37:12 +1000

    this?2

--- Train.py
+++ Train.py
@@ -276,11 +276,12 @@ def Train():
                 ## Gradient Penalty
 
                 eps = T.rand(samples.size(0), 1, 1, 1, device=device)
+                
                 with T.no_grad():
                     eps = eps.expand_as(Gen(noise))
-                    px_hat = Disc(x_hat)
                 x_hat = eps * samples + (1 - eps) * fake.detach()
                 x_hat.requires_grad = True
+                px_hat = Disc(x_hat)
                 grad = T.autograd.grad(
                                             outputs = px_hat.sum(),
                                             inputs = x_hat, 
